a95224076f0628af4ea36604d34aff27d87ba7ea
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-04-02 07:11:26 +0200

    subnets - tf-roboshop

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,12 +1,12 @@
+env="dev"
 vpc = {
   main = {
     cidr_block = "10.0.0.0/16"
-    subnets={
-      public={
+    subnets = {
+      public = {
         name="public"
         cidr_block=["10.0.0.0/24","10.0.1.0/24"]
       }
   }
 }
 }
-env="dev"
--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 module "vpc" {
   source = "git::https://github.com/awsdevopsb01/tf-module-vpc.git"
   for_each = var.vpc
-  cidr_block=each.value["cidr_block"]
-  subnets=each.value["subnets"]
+  cidr_block = each.value["cidr_block"]
+  subnets = each.value["subnets"]
   tags=local.tags
   env=var.env
-  name=var.name
 }
 
