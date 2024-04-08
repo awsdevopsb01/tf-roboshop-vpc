494996733d3669bc0bf7a14b233ddb275a67fd95
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-04-08 15:31:42 +0200

    test

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ output "vpc" {
   value = module.vpc
 }
 
-#module "app" {
-#  source = "git::https://github.com/awsdevopsb01/tf-module-app.git"
-#
-#  for_each = var.app
-#  instance_type=each.value["instance_type"]
-#  subnet_ids=element(lookup(lookup(lookup(lookup(module.vpc,"main",null ),"subnets",null),each.value["subnet_name"],null),"subnet_ids",null),0)
-#}
+module "app" {
+  source = "git::https://github.com/awsdevopsb01/tf-module-app.git"
+
+  for_each = var.app
+  instance_type=each.value["instance_type"]
+  subnet_ids=lookup(lookup(lookup(lookup(module.vpc,"main",null ),"subnets",null),each.value["subnet_name"],null),"subnet_ids",null)
+}
