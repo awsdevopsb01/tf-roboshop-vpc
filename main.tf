036ac42f80f62450ff53dfffd637f60d0bfbd741
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-04-12 06:17:25 +0200

    module docdb

--- main.tf
+++ main.tf
@@ -30,18 +30,20 @@ module "vpc" {
 #}
 
 
-#module "docdb" {
-#  source = "git::https://github.com/awsdevopsb01/tf-module-docdb.git"
-#
-#  for_each = var.docdb
-#  subnet_ids = lookup(lookup(lookup(lookup(module.vpc,"main",null ),"subnet_ids",null),each.value["subnet_name"],null),"subnet_ids",null)
-#  allow_db_cidr=lookup(lookup(lookup(lookup(module.vpc,"main",null ),"subnet_ids",null),each.value["allow_db_cidr"],null),"subnet_cidrs",null)
-#  engine_version = each.value["engine_version"]
-#
-#  env=var.env
-#  tags = local.tags
-#  vpc_id = local.vpc_id
-#  kms_arn = var.kms_arn
-#
-#}
+module "docdb" {
+  source = "git::https://github.com/awsdevopsb01/tf-module-docdb.git"
+
+  for_each = var.docdb
+  subnet_ids = lookup(lookup(lookup(lookup(module.vpc,"main",null ),"subnet_ids",null),each.value["subnet_name"],null),"subnet_ids",null)
+  allow_db_cidr=lookup(lookup(lookup(lookup(module.vpc,"main",null ),"subnet_ids",null),each.value["allow_db_cidr"],null),"subnet_cidrs",null)
+  engine_version = each.value["engine_version"]
+  instance_count = each.value["instance_count"]
+  instance_class = each.value["instance_class"]
+
+  env=var.env
+  tags = local.tags
+  vpc_id = local.vpc_id
+  kms_arn = var.kms_arn
+
+}
 
